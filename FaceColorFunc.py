a90bb09720737676d8ce0e3a3d144bb06202b6f2
BOB-o-rabanete <127680483+BOB-o-rabanete@users.noreply.github.com>
2025-12-31 05:56:29 +0000

    .3txt

--- FaceColorFunc.py
+++ FaceColorFunc.py
@@ -27,7 +27,7 @@ def get_face_mask(img: np.ndarray) -> np.ndarray:
             Same size as input, non-skin areas set to [128,128,128] (gray)
             None if no face was found
     """
-    # Initialize face mesh
+
     with mp_face_mesh.FaceMesh(
         static_image_mode=True,
         max_num_faces=1, 
@@ -40,7 +40,6 @@ def get_face_mask(img: np.ndarray) -> np.ndarray:
             print("🐢 ---> no face detected in image.")
             return None
 
-        # Get landmarks for the face
         h, w, _ = img.shape
         landmarks = results.multi_face_landmarks[0]
 
@@ -69,31 +68,19 @@ def get_face_mask(img: np.ndarray) -> np.ndarray:
                 for i in idxs
             ])
 
-        face_outline = get_points(face_outline_idx)#face_outline_idx
-        #left_eye = get_points(left_eye_idx)
-        #right_eye = get_points(right_eye_idx)
-        #lips = get_points(lips_idx)
+        face_outline = get_points(face_outline_idx)
 
 
         # ---- Create base mask ---- #
         mask = np.zeros((h, w), dtype=np.uint8)
-        cv2.fillPoly(mask, [face_outline], 255)  # fill outer face area
+        cv2.fillPoly(mask, [face_outline], 255)  
 
-        # Remove eyes and lips from mask #fais drastically
-        #cv2.fillPoly(mask, [left_eye], 0)
-        #cv2.fillPoly(mask, [right_eye], 0)
-        #cv2.fillPoly(mask, [lips], 0)
-
-        # Smooth edges a bit
         mask = cv2.GaussianBlur(mask, (11, 11), 0)
 
-        # Expand to 3 channels
         mask_3ch = cv2.merge([mask, mask, mask])
 
-        # Make gray background
         gray_bg = np.full_like(img, 128)
 
-        # Combine: if mask=255 → keep skin pixel; else → gray background
         mask_img = np.where(mask_3ch == 255, img, gray_bg)
 
         return mask_img
